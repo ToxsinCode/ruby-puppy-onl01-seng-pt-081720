0c072d3c97849538fb13071ffeb2a5fbb611e5f8
Jevoughn Wisdom <jevoughnwisdom@gmail.com>
2020-11-05 02:05:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,20 @@
 class Dog
   attr_accessor :name
   @@all = []
-  
+
   def initialize(name)
     @name = name
     @@all << self
   end
-  
+
   def self.all
     @@all
   end
-  
+
   def self.clear_all
     @@all.clear
   end
-  
+
   def self.print_all
     @@all.each do print
   end
